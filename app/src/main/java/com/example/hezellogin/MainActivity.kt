b7fc211d28c4632b7b1c259b378c112afc9032c9
ngangavic <ngangavictor10@gmail.com>
2019-10-15 11:41:42 +0300

    login fun

--- app/src/main/java/com/example/hezellogin/MainActivity.kt
+++ app/src/main/java/com/example/hezellogin/MainActivity.kt
@@ -1,26 +1,63 @@
 package com.example.hezellogin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editTextEmail: EditText
+    lateinit var editTextPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val editTextEmail=findViewById<EditText>(R.id.editTextEmail)
-        val editTextPassword=findViewById<EditText>(R.id.editTextPassword)
-        val buttonLogin=findViewById<Button>(R.id.buttonLogin)
-        val buttonRegister=findViewById<Button>(R.id.buttonRegister)
+        editTextEmail = findViewById<EditText>(R.id.editTextEmail)
+        editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
 
         buttonRegister.setOnClickListener {
-            startActivity(Intent(this,RegisterActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        buttonLogin.setOnClickListener {
+            login(editTextEmail.text.toString(), editTextPassword.text.toString())
         }
 
     }
+
+    //verify email
+    private fun verifyEmail(email: String): Boolean {
+        return email.contains("@") && email.endsWith("hezel.andahwa")
+    }
+
+    //verify password
+    private fun verifyPassword(pass: String): Boolean {
+        return pass.length >= 8
+    }
+
+    //login
+    private fun login(email: String, pass: String) {
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.requestFocus()
+            editTextEmail.setError("Cannot be empty")
+        } else if (TextUtils.isEmpty(pass)) {
+            editTextPassword.requestFocus()
+            editTextPassword.setError("Cannot be empty")
+        } else if (!verifyEmail(email)) {
+            editTextEmail.requestFocus()
+            editTextEmail.setError("Invalid email")
+        } else if (!verifyPassword(pass)) {
+            editTextPassword.requestFocus()
+            editTextPassword.setError("Invalid password")
+        } else {
+            Toast.makeText(this, "Login success", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
