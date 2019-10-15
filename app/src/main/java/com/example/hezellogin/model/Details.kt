4cff18a73de1fe10b50c2cb4b7de8e011e36ebef
ngangavic <ngangavictor10@gmail.com>
2019-10-15 11:53:15 +0300

    model

---
+++ app/src/main/java/com/example/hezellogin/model/Details.kt
@@ -0,0 +1,13 @@
+package com.example.hezellogin.model
+
+class Details {
+    var email: String? = null
+    var password: String? = null
+    constructor(email: String, password: String) {
+        this.email = email
+        this.password = password
+    }
+//    constructor(userName: String) {
+//        this.userName = userName
+//    }
+}
