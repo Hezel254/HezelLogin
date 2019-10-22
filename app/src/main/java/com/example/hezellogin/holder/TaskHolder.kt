0f133f8e8ba6de98b83d2efaad27c2715bf9a0cc
ngangavic <ngangavictor10@gmail.com>
2019-10-22 12:42:03 +0300

    task holder

--- app/src/main/java/com/example/hezellogin/holder/TaskHolder.kt
+++ app/src/main/java/com/example/hezellogin/holder/TaskHolder.kt
@@ -1,4 +1,25 @@
 package com.example.hezellogin.holder
 
-class TaskHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hezellogin.R
+import com.example.hezellogin.model.Task
+
+class TaskHolder(itemView: View, private val taskObject: List<Task>) :
+    RecyclerView.ViewHolder(itemView) {
+
+    var textViewName: TextView
+    var textViewDate: TextView
+    var textViewDesc: TextView
+    var cardViewTask: CardView
+
+    init {
+        textViewName = itemView.findViewById(R.id.textViewName)
+        textViewDate = itemView.findViewById(R.id.textViewDate)
+        textViewDesc = itemView.findViewById(R.id.textViewDesc)
+        cardViewTask = itemView.findViewById(R.id.cardViewTask)
+    }
+
 }
