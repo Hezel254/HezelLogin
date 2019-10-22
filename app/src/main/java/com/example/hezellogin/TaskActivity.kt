e99d1efb7a7d662e1cde0d055c8f6b4fefb64eaa
ngangavic <ngangavictor10@gmail.com>
2019-10-22 12:41:50 +0300

    task activity

--- app/src/main/java/com/example/hezellogin/TaskActivity.kt
+++ app/src/main/java/com/example/hezellogin/TaskActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hezellogin
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,7 +8,9 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hezellogin.adapter.TaskAdapter
 import com.example.hezellogin.helper.DbHelper
 import com.example.hezellogin.model.Task
 import kotlinx.android.synthetic.main.add_task.*
@@ -17,13 +20,42 @@ import java.util.*
 class TaskActivity : AppCompatActivity() {
     lateinit var rv_task: RecyclerView
     lateinit var dbHelper: DbHelper
+    private var taskLists: MutableList<Task>? = null
+    private var taskAdapter: TaskAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
         rv_task = findViewById(R.id.rv_task)
         dbHelper = DbHelper(this, null)
+        rv_task.layoutManager = LinearLayoutManager(this)
+        rv_task.setHasFixedSize(true)
+        taskLists = ArrayList()
+        taskAdapter = TaskAdapter(
+            this@TaskActivity,
+            taskLists as ArrayList<Task>
+        )
+        rv_task.setAdapter(taskAdapter)
 
+        loadItems()
+        taskAdapter!!.notifyDataSetChanged()
+    }
+
+    private fun loadItems() {
+        val cursor = dbHelper.getTask()
+        (taskLists as ArrayList<Task>).clear()
+        while (cursor.moveToNext()) {
+            val id = cursor.getInt(0)
+            val name = cursor.getString(1)
+            val desc = cursor.getString(2)
+            val date = cursor.getString(3)
+
+            var cart = Task(id, name, desc, date)
+
+            (taskLists as ArrayList<Task>).add(cart)
+            taskAdapter?.notifyDataSetChanged()
+        }
+        taskAdapter?.notifyDataSetChanged()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -63,6 +95,7 @@ class TaskActivity : AppCompatActivity() {
             dbHelper.inserttask(data)
             alertDialog.dismiss()
             alertDialog.cancel()
+            loadItems()
         }
     }
 
