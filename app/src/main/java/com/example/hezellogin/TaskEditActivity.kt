0bd202b402c7ee1ab1e458e790015d0b50297e19
ngangavic <ngangavictor10@gmail.com>
2019-10-22 13:29:14 +0300

    task edit activity

--- app/src/main/java/com/example/hezellogin/TaskEditActivity.kt
+++ app/src/main/java/com/example/hezellogin/TaskEditActivity.kt
@@ -1,12 +1,39 @@
 package com.example.hezellogin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hezellogin.helper.DbHelper
+import com.example.hezellogin.model.Task
 
 class TaskEditActivity : AppCompatActivity() {
+
+    lateinit var editTextName: EditText
+    lateinit var editTextDesc: EditText
+    lateinit var buttonUpdate: Button
+    lateinit var dbHelper: DbHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_edit)
+        editTextName = findViewById(R.id.editTextName)
+        editTextDesc = findViewById(R.id.editTextDesc)
+        buttonUpdate = findViewById(R.id.buttonUpdate)
+        dbHelper = DbHelper(this, null)
+
+        val id = intent.getStringExtra("id")
+        val name = intent.getStringExtra("name")
+        val desc = intent.getStringExtra("desc")
+
+        editTextName.setText(name)
+        editTextDesc.setText(desc)
+
+        buttonUpdate.setOnClickListener {
+            val data =
+                Task(id.toInt(), editTextName.text.toString(), editTextDesc.text.toString())
+            dbHelper.edittask(data)
+        }
+
     }
 }
