36b272f58f3b1bc89da1bbf8c9562b8099ba4b33
ngangavic <ngangavictor10@gmail.com>
2019-10-22 10:00:25 +0300

    recyclerview setup

--- app/src/main/java/com/example/hezellogin/TaskActivity.kt
+++ app/src/main/java/com/example/hezellogin/TaskActivity.kt
@@ -2,11 +2,14 @@ package com.example.hezellogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class TaskActivity : AppCompatActivity() {
+    lateinit var rv_task:RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
+        rv_task=findViewById(R.id.rv_task)
     }
 }
