b433306d3ec872405c8b9bb46057d2788416985f
ngangavic <ngangavictor10@gmail.com>
2019-10-22 10:49:45 +0300

    task menu setup

--- app/src/main/java/com/example/hezellogin/TaskActivity.kt
+++ app/src/main/java/com/example/hezellogin/TaskActivity.kt
@@ -2,6 +2,8 @@ package com.example.hezellogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskActivity : AppCompatActivity() {
@@ -11,5 +13,23 @@ class TaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
         rv_task=findViewById(R.id.rv_task)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_task,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add->{
+                //add task
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
