c463dbe5a5711a4a9f19cdee8c7d45dba0ed367e
ngangavic <ngangavictor10@gmail.com>
2019-10-22 12:41:35 +0300

    task adapter

--- app/src/main/java/com/example/hezellogin/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/hezellogin/adapter/TaskAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.hezellogin.adapter
 
-class TaskAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hezellogin.R
+import com.example.hezellogin.helper.DbHelper
+import com.example.hezellogin.holder.TaskHolder
+import com.example.hezellogin.model.Task
+
+class TaskAdapter (protected var context: Context, private val task: ArrayList<Task>) :
+    RecyclerView.Adapter<TaskHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
+        var viewHolder: TaskHolder? = null
+        val layoutView =
+            LayoutInflater.from(parent.context).inflate(R.layout.row_task, parent, false)
+        viewHolder = TaskHolder(layoutView, task)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return this.task.size
+    }
+
+    override fun onBindViewHolder(holder: TaskHolder, position: Int) {
+        val dbHelper = DbHelper(context, null)
+        holder.textViewName.setText(task[position].name)
+        holder.textViewDate.setText(task[position].date)
+        holder.textViewDesc.setText(task[position].description)
+        holder.cardViewTask.setOnLongClickListener {
+            //delete
+            dbHelper.deletetask(task[position].id.toString())
+            task.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, itemCount)
+            true
+        }
+    }
+
+
 }
